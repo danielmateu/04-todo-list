5a8bbfc3e94e708d0a6547e967cc47680035d8c0
Daniel Mateu <danielmateu86@gmail.com>
2022-11-04 15:23:53 +0100

    Prparamos la información que presentamos al usuario

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -9,15 +9,16 @@ const mostrarMenu = () => {
     console.log('   Selecciona una opción'.green)
     console.log('===========================\n'.green)
 
-    console.log(`1. Crear tarea`)
-    console.log(`2. Listar tarea`)
-    console.log(`3. Listar tareas completadas`)
-    console.log(`4. Listar tarea pendientes`)
-    console.log(`5. Completar tarea(s) `)
-    console.log(`6. Eliminar tarea`)
-    console.log(`0. Salir \n`)
+    console.log(`${'1.'.green} Crear tarea`)
+    console.log(`${'2.'.green} Listar tarea`)
+    console.log(`${'3.'.green} Listar tareas completadas`)
+    console.log(`${'4.'.green} Listar tarea pendientes`)
+    console.log(`${'5.'.green} Completar tarea(s) `)
+    console.log(`${'6.'.green} Eliminar tarea`)
+    console.log(`${'0.'.green} Salir \n`)
     
     
+
 }
 
 module.exports = {
