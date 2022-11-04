496b382382a45d6b8ca204d0cfe01530db353b31
Daniel Mateu <danielmateu86@gmail.com>
2022-11-04 18:14:41 +0100

    Transformando el objeto en un arreglo

--- models/tareas.js
+++ models/tareas.js
@@ -2,14 +2,34 @@ const Tarea = require("./tarea");
 
 
 class Tareas {
+    
     _listado = {};
 
-    constructor(){
+    get listadoArr() {
+
+        const listado = [];
+        /* Iterating over the object and pushing the values into an array. */
+        Object.keys(this._listado).forEach(key => {
+            const tarea = this._listado[key];
+            listado.push(tarea);
+        });
+
+        return listado;
+    }
+
+    /**
+     * It creates a new object.
+     */
+    constructor() {
         this._listado = {}
     }
 
-    crearTarea(desc = ''){
-        
+    /**
+     * The function crearTarea() creates a new task and adds it to the list of tasks.
+     * @param [desc] - The description of the task.
+     */
+    crearTarea(desc = '') {
+
         const tarea = new Tarea(desc);
 
         this._listado[tarea.id] = tarea;
